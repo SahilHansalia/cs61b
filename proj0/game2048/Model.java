2bf691cbaa9c75fc0d28e407256b7fac10770f4b
Sahil Hansalia <sahil.hansalia@berkeley.edu>
2017-09-19 13:52:14 -0700

    push to ssh

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -211,8 +211,13 @@ class Model extends Observable {
                         return;
                     }
                 }
+                if (_board[i][j] == null) {
+                    _gameOver = false;
+                    return;
+                }
             }
         }
+
         if (!canmerge(Side.NORTH) && !canmerge(Side.SOUTH)
             && !canmerge(Side.EAST) && !canmerge(Side.WEST)) {
             if (_maxScore < _score) {
