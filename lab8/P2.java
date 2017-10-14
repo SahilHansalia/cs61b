4de376a27d3df557624673441c04e61cb3f8195c
Sahil Hansalia <sahil.hansalia@berkeley.edu>
2017-10-13 23:23:14 -0700

    saving lab8

--- lab8/P2.java
+++ lab8/P2.java
@@ -1,6 +1,30 @@
+import com.sun.tools.javac.code.Attribute;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class P2 {
 
     public static void main(String... ignored) {
+        Scanner read = new Scanner(System.in);
+        ArrayList<String> in_line = new ArrayList<>();
+        ArrayList<String> pre_line = new ArrayList<>();
+        int pre = 0;
+        while (read.hasNext()) {
+            pre += 1;
+            pre_line.add(read.next());
+        }
+        read.nextLine();
+        int in = 0;
+        while (read.hasNext()) {
+            in += 1;
+            in_line.add(read.next());
+        }
+
+        //somehow use distances from end of each letter for
+        //either string to weigh???
+
+
 
 
     }
--- lab8/P3.java
+++ lab8/P3.java
@@ -1,10 +1,27 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class P3 {
 
     public static void main(String... ignored) {
+        Scanner read = new Scanner(System.in);
+        int i = read.nextInt();
+        int num = 1;
+        int len;
+        ArrayList<Integer> output = new ArrayList<Integer>();
+        for (int a = i; a > 0; a++) {
+            if (output.get(num) != output.get(num-1)) {
+                output.add(num);
+                num++;
+            }
+        }
 
 
 
+            System.out.print("The smallest good numeral of length " + i + "is" + output);
+
+
+        }
     }
-
 
 }
