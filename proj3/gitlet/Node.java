45e62bfbd1c701acfa8a0cd48dba3ee0d484f814
Sahil Hansalia <sahil.hansalia@berkeley.edu>
2017-11-21 17:18:34 -0800

    saving

---
+++ proj3/gitlet/Node.java
@@ -0,0 +1,8 @@
+package gitlet;
+
+/** class representing commits which can be thought of as
+ * nodes in the overall commit tree.
+ *  @author sahil
+ */
+public class Node {
+}
---
+++ proj3/gitlet/Tree.java
@@ -0,0 +1,7 @@
+package gitlet;
+
+/** class for tree structure representing all commits and relevant information..
+ *  @author sahil
+ */
+public class Tree {
+}
